92f9f6ea83e26784d075585acee5011468fffe92
AbidHasan1272 <abidhasan1272@gmail.com>
2025-02-22 21:32:53 +1000

    Added 2 codes for list exercises

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,20 @@
+numbers =[]
+NUMBER_COUNT=5
+for i in range(NUMBER_COUNT):
+    number=int(input(f"Enter number {i+1}: " ))
+    numbers.append(number)
+
+total=sum(numbers)
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The highest number is {max(numbers)}")
+print(f"The average of all this number is {total/len(numbers)}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username=input("Enter Your username:")
+if username in usernames:
+    print("Access Granted, Welcome Mate.")
+else:
+    print("Access Denied, Sorry mate")
