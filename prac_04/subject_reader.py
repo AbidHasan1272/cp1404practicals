8faad48d6ae8d7cedfb50268e0862a2329540423
AbidHasan1272 <abidhasan1272@gmail.com>
2025-02-22 21:06:55 +1000

    Completed subject reader code.

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -1,29 +1,29 @@
 """
 CP1404/CP5632 Practical
-Data file -> lists program
+Program: Data file -> lists program
+Student Name:Abid Hasan
+Date:22/02/2025
 """
-
 FILENAME = "subject_data.txt"
 
-
 def main():
-    data = load_data()
-    print(data)
-
+    subjects = load_data()
+    display_subject_details(subjects)
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
-    input_file = open(FILENAME)
-    for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
-    input_file.close()
+    data = []
+    with open(FILENAME, 'r') as file:
+        for line in file:
+            line = line.strip()
+            parts = line.split(',')
+            parts[2] = int(parts[2])
+            data.append(parts)
+    return data
 
+def display_subject_details(subjects):
+    """Display subject details in a formatted manner."""
+    for subject in subjects:  # Loop through each subject in the list
+        print(f"{subject[0]} is taught by {subject[1]:12} and has {subject[2]:35} students")
 
 main()
